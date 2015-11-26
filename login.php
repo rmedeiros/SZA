5bc5a8231f5b3786841047240b3ef201fa3aab3a
Raul Medeiros <rmedeirosperez@gmail.com>
2015-11-26 15:12:28 +0100

    first commit

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php 
+	if(!isset($_POST['eposta'])||!isset($_POST['pasahitza']))
+		echo"Errorea: Datuak ez dira ondo lortu";
+	if(!($erabiltzaileak=simplexml_load_file("data/erabiltzaileak.xml")))
+			{
+				echo('<p>Errore bat gertatu da erabiltzaileak irakurtzean. Barkatu eragozpenak</p>');
+			}
+		
+				
+	foreach($erabiltzaileak->erabiltzailea as $erabiltzailea){
+ 			if($erabiltzailea->eposta==$_POST['eposta'] && $erabiltzailea->pasahitza==$_POST['pasahitza']){
+				header('Location: ');
+			}	
+			
+		}
+	echo"<p style='color:red>Sartu dituzun datuak ez dira zuzenak, saiatu berriro</p>";	
+
+?>
